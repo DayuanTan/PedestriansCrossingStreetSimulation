471d58a3a86598fe8aff0ae6604ec160d2a2aa36
BigCircle <zhuceafter2015@163.com>
2021-01-27 02:04:11 -0500

    try find new position

--- backend/run.py
+++ backend/run.py
@@ -1,5 +1,5 @@
 import PedCross
-import Circles
+from Circles import Circles
 import operator
 
 
@@ -21,19 +21,25 @@ print("total_size: ", total_size, " ", len(all_peds))
 
 # all_peds_sorted_by_y = sorted(all_peds, key=operator.attrgetter('y'), reverse=True)
 all_peds_sorted_by_y = sorted(all_peds, key=operator.attrgetter('y'))
-print(all_peds_sorted_by_y)
+print(all_peds_sorted_by_y, "\n")
 
 for i in range(total_size):
     newx = all_peds_sorted_by_y[i].x + all_peds_sorted_by_y[i].velocity 
     newy = all_peds_sorted_by_y[i].y
     conflict = all_peds_sorted_by_y[i].is_newspace_conflict(newx, newy , all_peds_sorted_by_y[i+1:])
+    print("conflict: ", len(conflict))
     if (len(conflict) == 0):
         all_peds_sorted_by_y[i].x = newx
         all_peds_sorted_by_y[i].y = newy
     else:
         for in_front_ped in all_peds_sorted_by_y[i+1:]:
+            print("test: ", Circles.get_intersections(1,1,1,2,1,1), "\n")
             intersection = Circles.get_intersections(all_peds_sorted_by_y[i].x, all_peds_sorted_by_y[i].y, all_peds_sorted_by_y[i].velocity * SIMU_STEP_TIME, in_front_ped.x, in_front_ped.y, in_front_ped.radius + all_peds_sorted_by_y[i].radius)
+            print(intersection)
             newx = intersection[2]
             newy = intersection[3]
             conflict = all_peds_sorted_by_y[i].is_newspace_conflict(newx, newy , all_peds_sorted_by_y[:i])
             print(conflict)
+            if (len(conflict) == 0):
+                all_peds_sorted_by_y[i].x = newx
+                all_peds_sorted_by_y[i].y = newy
